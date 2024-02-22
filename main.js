f192cec1ed99c477b7d90a657bf5c1959e281019
made junes <made.junes@gmail.com>
2024-02-23 00:35:42 +0800

    send chat from input to container

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import "./style.css";
-import { initWidget } from "./widget";
 
-const widgetEl = document.querySelector("#widget-app");
+function sendMyMessage(theMessage) {
+  const chatContainerEl = document.querySelector(
+    "#widget-app .widget-chat-container"
+  );
 
-initWidget(widgetEl);
+  chatContainerEl.innerHTML += `
+      <div class="message-wrapper me">
+            <div class="message-inner">
+              <div class="message">${theMessage}</div>
+            </div>
+          </div>
+    `;
+}
+
+(function listenToInput() {
+  const textAreaEl = document.querySelector(
+    "#widget-app .widget-form textarea"
+  );
+  textAreaEl.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      // console.log(textAreaEl.value);
+      sendMyMessage(textAreaEl.value);
+      textAreaEl.value = "";
+    }
+  });
+})();
