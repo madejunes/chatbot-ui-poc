6d3a657e864525e8b063a4bd20285585c43686d7
made junes <made.junes@gmail.com>
2024-02-22 21:54:41 +0800

    basic chat and input styling

--- main.js
+++ main.js
@@ -2,10 +2,5 @@ import "./style.css";
 import { initWidget } from "./widget";
 
 const widgetEl = document.querySelector("#widget-app");
-widgetEl.innerHTML = `
-  <div class="widget-inner">
-    ini lho
-  </div>
-`;
 
 initWidget(widgetEl);
